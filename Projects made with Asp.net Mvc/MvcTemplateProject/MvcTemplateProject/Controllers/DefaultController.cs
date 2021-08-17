c83deaf01708dc629541f1e25f9a2aa2fa8a9882
zaferaslan <zafer.aslan@ogr.dpu.edu.tr>
2021-08-17 21:05:17 +0300

    Static Website Project
    Static Website Project with ASP.NET MVC and Bootstrap Theme

---
+++ Projects made with Asp.net Mvc/MvcTemplateProject/MvcTemplateProject/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTemplateProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Takim()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Portfoy()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
